01cc3998aec58a0c7f167013d113c6025facb2f6
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-13 19:51:20 +0800

    Initial auth route

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
 const server = express();
+import authRouter from "./routes/auth.js";
+
+server.use("/auth", authRouter);
 
 server.listen(5000, () => console.log("Listening on port 5000"));
