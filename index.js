e259de2a507335752cc839b5be6d3e78cf99a733
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-13 20:19:24 +0800

    Add signup route, express-validator

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 const server = express();
 import authRouter from "./routes/auth.js";
 
+server.use(express.json());
 server.use("/auth", authRouter);
 
 server.listen(5000, () => console.log("Listening on port 5000"));
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,28 @@
 import express from "express";
 const router = express.Router();
+import { check, validationResult } from "express-validator";
 
-router.get("/", (req, res) => {
-  res.send("Hello from auth");
+const validations = [
+  check("email", "Please enter a valid email address").isEmail(),
+  check("password", "Password must be more than 4 characters").isLength({
+    min: 5,
+  }),
+];
+
+router.post("/signup", validations, (req, res) => {
+  const { email, password } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  res.json({
+    email,
+    password,
+  });
 });
 
 export default router;
